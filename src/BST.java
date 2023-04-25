67764247f4bb13977adf6e8c155e9b89d33f69b5
piyush <piyush.yadav@ripplr.in>
2023-04-26 00:56:45 +0530

    convert-array-to-bst

--- src/BST.java
+++ src/BST.java
@@ -10,6 +10,11 @@ class Nodes{
         this.right=null;
     }
 }
+
+
+
+//check tree is BST or not
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!1
 public class BST {
     public static boolean BST(Nodes root, long min, long max){
         if(root==null){
@@ -34,3 +39,33 @@ public class BST {
         System.out.println(res);
     }
 }
+
+
+
+//convert arr or list to bst
+//!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
+//sort the array if array is not sorted then apply binary search and find the nodes.
+
+
+class Solution {
+    public static TreeNode bst(int[] arr, int l, int r){
+        if(l>r){
+            return null;
+        }
+        int mid=(l+r)/2;
+        TreeNode res=new TreeNode(arr[mid]);
+        res.left=bst(arr,l,mid-1);
+        res.right=bst(arr,mid+1,r);
+        return res;
+    }
+    public TreeNode sortedArrayToBST(int[] nums) {
+        if(nums.length==0){
+            return null;
+        }
+        TreeNode res=bst(nums,0,nums.length-1);
+        return res;
+    }
+}
+
+
+
